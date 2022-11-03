21fdaf6450e063c033a8f3d5f43b677c9f276103
cbozan <71611710+cbozan@users.noreply.github.com>
2022-11-03 17:28:56 +0300

    rest api post operations completed

--- src/task-2/restAPIService.py
+++ src/task-2/restAPIService.py
@@ -17,8 +17,36 @@ class DefaultParameters(Resource):
         f = open("default_parameters.json", "r")
         default_parameters = json.loads(f.read())
         f.close()
+        return default_parameters, 200
+    
+    def post(self):
+        f = open("default_parameters.json", "r")
+        default_parameters = json.loads(f.read())
+        f.close()
+        
+        hl = request.args.get('hl')
+        tz = request.args.get('tz')
+        pn = request.args.get('pn')
+
+        if hl== None:
+            hl = default_parameters.get('hl')
+            
+        if tz == None:
+            tz = default_parameters.get('tz')
+            
+        if pn == None:
+            pn = default_parameters.get('pn')
+
+        default_parameters['hl'] = hl
+        default_parameters['tz'] = tz
+        default_parameters['pn'] = pn
+
+        with open("default_parameters.json", "w") as outfile:
+            json.dump(default_parameters, outfile)
+        outfile.close()
 
         return default_parameters, 200
+
 
 api.add_resource(DefaultParameters, "/default_parameters")
 
